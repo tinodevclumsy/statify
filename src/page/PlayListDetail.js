f826698611654b51ed7033845150c03c3d090336
tinodevclumsy <seungjun.dev@gmail.com>
2024-05-17 12:41:59 -0700

    implemented search function for album and track

--- src/page/PlayListDetail.js
+++ src/page/PlayListDetail.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getPlayListDetail } from "../api/SpotifyAPI";
 import PlayListCard from "../components/PlayListCard";
@@ -25,37 +25,68 @@ const Background = styled.div`
   background-size: cover;
 `;
 
+const ListNav = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 15px 0;
+`;
+
 const PlayListDetail = () => {
   const { id } = useParams();
+  const NUM_PER_PAGE = 10;
 
   const [detail, setDetail] = useState();
   const [group, setGroup] = useState();
+  const [search, setSearch] = useState("");
+  const [page, setPage] = useState(0)
 
   useEffect(() => {
     getPlayListDetail({ id }).then((res) => {
       setDetail(res);
-      setGroup([...filterTracks(res.tracks.items)]);
+      setGroup([...groupAlbums(res.tracks.items)]);
     });
-  }, []);
+  }, [id]);
 
-  function filterTracks(tracks) {
+  function groupAlbums(tracks) {
     return tracks.reduce((arr, crr) => {
       const id = crr.track.album.id;
-      const data = arr.get(id)
-      const track = { name: crr.track.name, artists: crr.track.artists }
-      const thumbnail = crr.track.album.images
-      if(!data) {
+      const data = arr.get(id);
+      const track = { name: crr.track.name, artists: crr.track.artists };
+      const thumbnail = crr.track.album.images;
+
+      if (!data) {
         arr.set(id, {
           id,
+          name: crr.track.album.name,
           thumbnail,
-          tracks: [track]
-        })
+          tracks: [track],
+        });
       } else {
-        data.tracks.push(track)
+        data.tracks.push(track);
       }
 
-      return arr
+      return arr;
     }, new Map());
+  }
+
+  function filterAlbums() {
+    if (search) {
+      const filtered = group.filter((ele) => {
+        if (
+          ele[1].name.toLowerCase().includes(search.toLowerCase()) ||
+          ele[1].tracks.filter((ele) =>
+            ele.name.toLowerCase().includes(search.toLowerCase())
+          ).length
+        ) {
+          return true;
+        }
+        return false;
+      });
+
+      return filtered.slice(page, page + NUM_PER_PAGE);
+    }
+
+    return group.slice(page, page + NUM_PER_PAGE);
   }
 
   return (
@@ -70,7 +101,13 @@ const PlayListDetail = () => {
           <div style={{ textAlign: "center", padding: "20px" }}>
             <LinkButton>Create a Board</LinkButton>
           </div>
-          <TrackList data={group} onTrackClick={() => {}} />
+          <ListNav>
+            <p style={{ color: "#fff" }}>
+              Total Albums: {group.length} / Total Tracks: {detail.tracks.total}
+            </p>
+            <input onChange={(e) => setSearch(e.target.value)} />
+          </ListNav>
+          <TrackList data={filterAlbums()} onTrackClick={() => {}} />
           <Background
             style={{ backgroundImage: `url(${detail.images[0].url})` }}
           />
