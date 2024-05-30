6f2fa83e91f259a24f6a5853df2c9840b4700242
tinodevclumsy <seungjun.dev@gmail.com>
2024-05-29 22:16:25 -0700

    implemented drag, drop and swap on board

--- src/page/Board.js
+++ src/page/Board.js
@@ -1,23 +1,37 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import styled from "styled-components";
 import AlbumContext from "../context/AlbumContext";
 
-const Nav = styled.div`
-  margin-bottom: 35px;
+const Panel = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  background-color: ${(props) => props.theme.grey};
+  width: 350px;
+  height: 100vh;
+  z-index: 1;
+  transform: ${(props) => (props.open ? "translateX(0)" : "translateX(-100%)")};
+  transition: 0.2s ease-in-out all;
+  -webkit-transition: 0.2s ease-in-out all;
 `;
 
 const Container = styled.div`
-  max-width: 750px;
-  margin: 100px auto;
+  width: 100%;
+  margin: 75px auto 0;
+  display: flex;
+  min-height: calc(100vh - 75px);
+  align-items: center;
 `;
 
 const BoardContainer = styled.div`
-  /* display: grid;
-  margin: auto;
-  grid-template-columns: repeat(5, 1fr);
-  grid-gap: 10px;
-  padding: 10px; */
+  width: 100%;
+  padding: 20px;
+`;
+
+const Frame = styled.div`
   display: flex;
+  max-width: 800px;
+  margin: auto;
   flex-wrap: wrap;
   gap: 10px;
   background-color: ${(props) => props.theme.grey};
@@ -34,11 +48,19 @@ const PrimaryCell = styled.div`
   grid-column: span 6 / 5;
 `;
 
+const Search = styled.input`
+  width: 100%;
+  padding: 10px;
+`;
+
 const Board = () => {
   const { albums, setAlbums } = useContext(AlbumContext);
+  const [swapIndex, setSwapIndex] = useState(-1);
+  const [panel, setPanel] = useState(false);
 
   useEffect(() => {
     const selected = albums.length;
+
     if (selected < 42) {
       setAlbums(albums.concat(Array(42 - selected).fill(null)));
     }
@@ -59,26 +81,66 @@ const Board = () => {
     return 10;
   };
 
+  const handleDragStart = (index) => {
+    setSwapIndex(index);
+  };
+
+  const handleDrop = (e, index) => {
+    e.stopPropagation();
+    e.preventDefault();
+
+    swapAlbums(swapIndex, index);
+  };
+
+  const handleDragOver = (e) => {
+    e.preventDefault();
+    return false;
+  };
+
+  const swapAlbums = (start, end) => {
+    const copied = [...albums];
+    const temp = copied[start];
+    copied[start] = copied[end];
+    copied[end] = temp;
+
+    setAlbums(copied);
+  };
+
+  const onCellClick = () => {
+    setPanel(!panel);
+  };
+
   return (
-    <Container>
-      <Nav>
-        <input onChange={(e) => {}} />
-      </Nav>
-      <BoardContainer>
-        {albums.map((item, key) => {
-          return (
-            <PrimaryCell
-              col={getColumns(key)}
-              spacing={(getColumns(key) - 1) * 10}
-              key={item ? item.name : `blank-${key}`}
-              style={item && { background: `url(${item.thumbnail[0].url})` }}
-            >
-              {/* <img src={item} /> */}
-            </PrimaryCell>
-          );
-        })}
-      </BoardContainer>
-    </Container>
+    <>
+      <Panel open={panel}>
+        <Search onChange={(e) => {}} />
+      </Panel>
+      <Container>
+        <BoardContainer>
+          <Frame>
+            {albums.map((item, key) => {
+              return (
+                <PrimaryCell
+                  col={getColumns(key)}
+                  spacing={(getColumns(key) - 1) * 10}
+                  key={item ? item.name : `blank-${key}`}
+                  draggable={true}
+                  onDragStart={() => handleDragStart(key)}
+                  onDrop={(e) => handleDrop(e, key)}
+                  onDragOver={handleDragOver}
+                  style={
+                    item && { background: `url(${item.thumbnail[0].url})` }
+                  }
+                  onClick={onCellClick}
+                >
+                  {/* <img src={item} /> */}
+                </PrimaryCell>
+              );
+            })}
+          </Frame>
+        </BoardContainer>
+      </Container>
+    </>
   );
 };
 
